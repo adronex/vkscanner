72f8720331f4f86d2963dbbd13da0cb475d5bfe5
Sinitskiy Pavel <sinitskiy@kpr-it.ru>
2017-01-18 21:28:05 +0300

    Initial commit

---
+++ src/main/kotlin/vkscanner/Application.kt
@@ -0,0 +1,46 @@
+package vkscanner
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+
+/**
+ * Created on 17.01.2017;
+ * @author p.sinitskiy (adronex303@gmail.com);
+ * @since 1.0.
+ */
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = arrayOf(DataSourceAutoConfiguration::class))
+open class Application : SpringBootServletInitializer() {
+
+    @Bean
+    open fun mapperForKotlinTypes(): MappingJackson2HttpMessageConverter {
+        return MappingJackson2HttpMessageConverter().apply { objectMapper = jacksonMapper }
+    }
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder =
+            application.sources(Application::class.java)
+
+    companion object {
+
+        val jacksonMapper = ObjectMapper().registerKotlinModule()
+                .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+                .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+
+        @Throws(Exception::class)
+        @JvmStatic fun main(args: Array<String>) {
+            println("starting application...")
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
---
+++ src/main/kotlin/vkscanner/VkApiWhore.kt
@@ -0,0 +1,14 @@
+package vkscanner
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created on 17.01.2017;
+ * @author p.sinitskiy (adronex303@gmail.com);
+ * @since 1.0.
+ */
+
+@RestController
+class VkApiWhore {
+
+}
